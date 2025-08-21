7c0d395e1f2b2a68c32dc70b047d9d5302383d87
RalfBarkow <ralf.barkow@me.com>
2025-08-21 19:10:53 +0200

    chore(format): normalize Elm formatting
      npm run format

--- src/Config.elm
+++ src/Config.elm
@@ -7,40 +7,136 @@ import Model exposing (..)
 -- CONFIG
 
 
-homeMapName = "DM6 Elm"
-version = "0.2-snapshot"
-date = "Aug 21, 2025"
-newTopicPos = Point 178 180
+homeMapName =
+    "DM6 Elm"
 
-mainFont = "sans-serif"
-toolbarFontSize = 14
-contentFontSize = 13
-footerFontSize = 13
 
-topicWidth = 156
-topicHeight = 28 -- also width/height of square icon box
-topicW2 = topicWidth / 2
-topicH2 = topicHeight / 2
-topicSize = Size topicWidth topicHeight
-topicLabelWeight = "bold" -- or "normal"
-topicDetailSize = Size
-  (topicWidth - topicHeight) -- detail width does not include icon box
-  (topicLineHeight * contentFontSize + 2 * (topicDetailPadding + topicBorderWidth))
-topicDetailMaxWidth = 300
-topicDetailPadding = 8
-topicLineHeight = 1.5
-topicDefaultText = "New Topic"
-topicIconSize = 16
-topicBorderWidth = 1
-topicRadius = 7
+version =
+    "0.2-snapshot"
 
-assocWith = 1.5
-assocRadius = 14 -- should not bigger than half topicSize height
-assocColor = "black"
-assocDelayMillis = 200
 
-whiteBoxRange = Size 250 150
-whiteBoxRadius = 14
-whiteBoxPadding = 12
+date =
+    "Aug 21, 2025"
 
-blackBoxOffset = 5
+
+newTopicPos =
+    Point 178 180
+
+
+mainFont =
+    "sans-serif"
+
+
+toolbarFontSize =
+    14
+
+
+contentFontSize =
+    13
+
+
+footerFontSize =
+    13
+
+
+topicWidth =
+    156
+
+
+topicHeight =
+    28
+
+
+
+-- also width/height of square icon box
+
+
+topicW2 =
+    topicWidth / 2
+
+
+topicH2 =
+    topicHeight / 2
+
+
+topicSize =
+    Size topicWidth topicHeight
+
+
+topicLabelWeight =
+    "bold"
+
+
+
+-- or "normal"
+
+
+topicDetailSize =
+    Size
+        (topicWidth - topicHeight)
+        -- detail width does not include icon box
+        (topicLineHeight * contentFontSize + 2 * (topicDetailPadding + topicBorderWidth))
+
+
+topicDetailMaxWidth =
+    300
+
+
+topicDetailPadding =
+    8
+
+
+topicLineHeight =
+    1.5
+
+
+topicDefaultText =
+    "New Topic"
+
+
+topicIconSize =
+    16
+
+
+topicBorderWidth =
+    1
+
+
+topicRadius =
+    7
+
+
+assocWith =
+    1.5
+
+
+assocRadius =
+    14
+
+
+
+-- should not bigger than half topicSize height
+
+
+assocColor =
+    "black"
+
+
+assocDelayMillis =
+    200
+
+
+whiteBoxRange =
+    Size 250 150
+
+
+whiteBoxRadius =
+    14
+
+
+whiteBoxPadding =
+    12
+
+
+blackBoxOffset =
+    5
