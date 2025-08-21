00953f8cc0400257fa5ad35c1bd6d04954815f54
RalfBarkow <ralf.barkow@me.com>
2025-08-21 15:30:23 +0200

    chore(format): normalize Elm formatting
      elm-format src/                         # format all *.elm files in the src directory

--- src/Boxing.elm
+++ src/Boxing.elm
@@ -1,14 +1,15 @@
 module Boxing exposing (boxContainer, unboxContainer)
 
-import Model exposing (..)
 import Dict
+import Model exposing (..)
 
 
 
 -- MODEL
 
 
-type alias TransferFunc = MapItems -> MapItems -> Model -> MapItems
+type alias TransferFunc =
+    MapItems -> MapItems -> Model -> MapItems
 
 
 
@@ -17,24 +18,27 @@ type alias TransferFunc = MapItems -> MapItems -> Model -> MapItems
 
 boxContainer : MapId -> MapId -> Model -> Maps
 boxContainer containerId targetMapId model =
-  transferContent containerId targetMapId boxItems model
+    transferContent containerId targetMapId boxItems model
 
 
 unboxContainer : MapId -> MapId -> Model -> Maps
 unboxContainer containerId targetMapId model =
-  transferContent containerId targetMapId unboxItems model
+    transferContent containerId targetMapId unboxItems model
 
 
 transferContent : MapId -> MapId -> TransferFunc -> Model -> Maps
 transferContent containerId targetMapId transferFunc model =
-  case getMap containerId model.maps of
-    Just containerMap ->
-      model.maps |> updateMaps
-        targetMapId
-        (\targetMap ->
-          { targetMap | items = transferFunc containerMap.items targetMap.items model }
-        )
-    Nothing -> model.maps
+    case getMap containerId model.maps of
+        Just containerMap ->
+            model.maps
+                |> updateMaps
+                    targetMapId
+                    (\targetMap ->
+                        { targetMap | items = transferFunc containerMap.items targetMap.items model }
+                    )
+
+        Nothing ->
+            model.maps
 
 
 {-| Transfer function, Boxing.
@@ -43,16 +47,22 @@ Returns the updated target items.
 -}
 boxItems : MapItems -> MapItems -> Model -> MapItems
 boxItems containerItems targetItems model =
-  containerItems |> Dict.values |> List.foldr
-    (\containerItem targetItemsAcc ->
-      let
-        items = hideItem_ containerItem.id targetItemsAcc model
-      in
-      case getMapIfExists containerItem.id model.maps of
-        Just map -> boxItems map.items items model
-        Nothing -> items
-    )
-    targetItems
+    containerItems
+        |> Dict.values
+        |> List.foldr
+            (\containerItem targetItemsAcc ->
+                let
+                    items =
+                        hideItem_ containerItem.id targetItemsAcc model
+                in
+                case getMapIfExists containerItem.id model.maps of
+                    Just map ->
+                        boxItems map.items items model
+
+                    Nothing ->
+                        items
+            )
+            targetItems
 
 
 {-| Transfer function, Unboxing.
@@ -61,79 +71,106 @@ Returns the updated target items.
 -}
 unboxItems : MapItems -> MapItems -> Model -> MapItems
 unboxItems containerItems targetItems model =
-  containerItems |> Dict.values |> List.filter isVisible |> List.foldr
-    (\containerItem targetItemsAcc ->
-      if isMapAssoc containerItem then
-        let
-          (items, abort) = unboxTopic containerItem targetItemsAcc model
-        in
-        if not abort then
-          case getMapIfExists containerItem.id model.maps of
-            Just map -> unboxItems map.items items model
-            Nothing -> items
-        else
-          items
-      else
-        unboxAssoc containerItem targetItemsAcc
-    )
-    targetItems
+    containerItems
+        |> Dict.values
+        |> List.filter isVisible
+        |> List.foldr
+            (\containerItem targetItemsAcc ->
+                if isMapAssoc containerItem then
+                    let
+                        ( items, abort ) =
+                            unboxTopic containerItem targetItemsAcc model
+                    in
+                    if not abort then
+                        case getMapIfExists containerItem.id model.maps of
+                            Just map ->
+                                unboxItems map.items items model
+
+                            Nothing ->
+                                items
+
+                    else
+                        items
+
+                else
+                    unboxAssoc containerItem targetItemsAcc
+            )
+            targetItems
 
 
 {-| Returns the target item to reveal that corresponds to the container item.
 Part of unboxing. FIXDOC
 -}
-unboxTopic : MapItem -> MapItems -> Model -> (MapItems, Bool)
+unboxTopic : MapItem -> MapItems -> Model -> ( MapItems, Bool )
 unboxTopic containerItem targetItems model =
-  let
-    (topicToInsert, abort) =
-      case targetItems |> Dict.get containerItem.id of
-        Just item ->
-          -- abort further unboxing if view item exists (= was unboxed before) and is set to
-          -- BlackBox or WhiteBox
-          ({ item | hidden = False }, isAbort item)
-        Nothing ->
-          -- by default (when no view item exists) an unboxed container will also be unboxed
-          if hasMap containerItem.id model.maps then
-            (setUnboxed containerItem, False)
-          else
-            (containerItem, False)
-    assocToInsert = targetAssocItem containerItem.parentAssocId targetItems
-  in
-  ( targetItems
-    |> Dict.insert topicToInsert.id topicToInsert
-    |> Dict.insert assocToInsert.id assocToInsert
-  , abort
-  )
+    let
+        ( topicToInsert, abort ) =
+            case targetItems |> Dict.get containerItem.id of
+                Just item ->
+                    -- abort further unboxing if view item exists (= was unboxed before) and is set to
+                    -- BlackBox or WhiteBox
+                    ( { item | hidden = False }, isAbort item )
+
+                Nothing ->
+                    -- by default (when no view item exists) an unboxed container will also be unboxed
+                    if hasMap containerItem.id model.maps then
+                        ( setUnboxed containerItem, False )
+
+                    else
+                        ( containerItem, False )
+
+        assocToInsert =
+            targetAssocItem containerItem.parentAssocId targetItems
+    in
+    ( targetItems
+        |> Dict.insert topicToInsert.id topicToInsert
+        |> Dict.insert assocToInsert.id assocToInsert
+    , abort
+    )
 
 
 unboxAssoc : MapItem -> MapItems -> MapItems
 unboxAssoc containerItem targetItems =
-  let
-    assocToInsert = targetAssocItem containerItem.id targetItems
-  in
-  targetItems
-    |> Dict.insert assocToInsert.id assocToInsert
+    let
+        assocToInsert =
+            targetAssocItem containerItem.id targetItems
+    in
+    targetItems
+        |> Dict.insert assocToInsert.id assocToInsert
 
 
 setUnboxed : MapItem -> MapItem
 setUnboxed item =
-  { item | props =
-    case item.props of
-      MapTopic props -> MapTopic { props | displayMode = Container Unboxed }
-      MapAssoc props -> MapAssoc props
-  }
+    { item
+        | props =
+            case item.props of
+                MapTopic props ->
+                    MapTopic { props | displayMode = Container Unboxed }
+
+                MapAssoc props ->
+                    MapAssoc props
+    }
 
 
 isAbort : MapItem -> Bool
 isAbort item =
-  case item.props of
-    MapTopic props ->
-      case props.displayMode of
-        Container BlackBox -> True
-        Container WhiteBox -> True
-        Container Unboxed -> False
-        Monad _ -> False
-    MapAssoc _ -> False
+    case item.props of
+        MapTopic props ->
+            case props.displayMode of
+                Container BlackBox ->
+                    True
+
+                Container WhiteBox ->
+                    True
+
+                Container Unboxed ->
+                    False
+
+                Monad _ ->
+                    False
+
+        MapAssoc _ ->
+            False
 
 
 {-| Returns the target item to reveal that corresponds to the container item.
@@ -141,6 +178,9 @@ Part of unboxing. FIXDOC
 -}
 targetAssocItem : Id -> MapItems -> MapItem
 targetAssocItem assocId targetItems =
-  case targetItems |> Dict.get assocId of
-    Just item -> { item | hidden = False }
-    Nothing -> MapItem assocId False (MapAssoc AssocProps) -1 False
+    case targetItems |> Dict.get assocId of
+        Just item ->
+            { item | hidden = False }
+
+        Nothing ->
+            MapItem assocId False (MapAssoc AssocProps) -1 False
--- src/Search.elm
+++ src/Search.elm
@@ -1,16 +1,15 @@
-module Search exposing (viewSearchInput, viewResultMenu, closeResultMenu, updateSearch)
+module Search exposing (closeResultMenu, updateSearch, viewResultMenu, viewSearchInput)
 
 import Config exposing (..)
+import Dict
+import Html exposing (Attribute, Html, div, input, text)
+import Html.Attributes exposing (attribute, style, value)
+import Html.Events exposing (on, onInput)
+import Json.Decode as D
 import Model exposing (..)
 import Storage exposing (storeModel)
-import Utils exposing (..)
-
-import Dict
-import Html exposing (Html, Attribute, div, text, input)
-import Html.Attributes exposing (value, style, attribute)
-import Html.Events exposing (onInput, on)
-import Json.Decode as D
 import String exposing (fromInt)
+import Utils exposing (..)
 
 
 
@@ -19,171 +18,216 @@ import String exposing (fromInt)
 
 viewSearchInput : Model -> Html Msg
 viewSearchInput model =
-  div
-    []
-    [ div
-      []
-      [ text "Search" ]
-    , input
-      ( [ value model.searchText
-        , onInput (Search << SearchInput)
+    div
+        []
+        [ div
+            []
+            [ text "Search" ]
+        , input
+            ([ value model.searchText
+             , onInput (Search << SearchInput)
+             ]
+                ++ searchInputStyle
+            )
+            []
         ]
-        ++searchInputStyle
-      )
-      []
-    ]
 
 
 searchInputStyle : List (Attribute Msg)
 searchInputStyle =
-  [ style "width" "100px" ]
+    [ style "width" "100px" ]
 
 
 viewResultMenu : Model -> List (Html Msg)
 viewResultMenu model =
-  case model.searchMenu of
-    ResultOpen _ ->
-      [ div
-        ( [ on "click" (itemDecoder ClickItem)
-          , on "mouseover" (itemDecoder OverItem)
-          , on "mouseout" (itemDecoder OutItem)
-          , stopPropagationOnMousedown NoOp
-          ]
-          ++ resultMenuStyle
-        )
-        ( model.searchResult |> List.map
-          (\id ->
-            case getTopicInfo id model of
-              Just topic ->
-                div
-                  ( [ attribute "data-id" (fromInt topic.id) ]
-                    ++ resultItemStyle topic.id model
-                  )
-                  [ text topic.text ]
-              Nothing -> text "??"
-          )
-        )
-      ]
-    ResultClosed -> []
+    case model.searchMenu of
+        ResultOpen _ ->
+            [ div
+                ([ on "click" (itemDecoder ClickItem)
+                 , on "mouseover" (itemDecoder OverItem)
+                 , on "mouseout" (itemDecoder OutItem)
+                 , stopPropagationOnMousedown NoOp
+                 ]
+                    ++ resultMenuStyle
+                )
+                (model.searchResult
+                    |> List.map
+                        (\id ->
+                            case getTopicInfo id model of
+                                Just topic ->
+                                    div
+                                        ([ attribute "data-id" (fromInt topic.id) ]
+                                            ++ resultItemStyle topic.id model
+                                        )
+                                        [ text topic.text ]
+
+                                Nothing ->
+                                    text "??"
+                        )
+                )
+            ]
+
+        ResultClosed ->
+            []
 
 
 itemDecoder : (Id -> SearchMsg) -> D.Decoder Msg
 itemDecoder msg =
-  D.map Search <| D.map msg
-    ( D.at ["target", "dataset", "id"] D.string
-      |> D.andThen strToIntDecoder
-    )
+    D.map Search <|
+        D.map msg
+            (D.at [ "target", "dataset", "id" ] D.string
+                |> D.andThen strToIntDecoder
+            )
 
 
 resultMenuStyle : List (Attribute Msg)
 resultMenuStyle =
-  [ style "position" "absolute"
-  , style "top" "144px"
-  , style "width" "240px"
-  , style "padding" "3px 0"
-  , style "font-size" <| fromInt contentFontSize ++ "px"
-  , style "line-height" "2"
-  , style "white-space" "nowrap"
-  , style "background-color" "white"
-  , style "border" "1px solid lightgray"
-  , style "z-index" "2"
-  ]
+    [ style "position" "absolute"
+    , style "top" "144px"
+    , style "width" "240px"
+    , style "padding" "3px 0"
+    , style "font-size" <| fromInt contentFontSize ++ "px"
+    , style "line-height" "2"
+    , style "white-space" "nowrap"
+    , style "background-color" "white"
+    , style "border" "1px solid lightgray"
+    , style "z-index" "2"
+    ]
 
 
 resultItemStyle : Id -> Model -> List (Attribute Msg)
 resultItemStyle topicId model =
-  let
-    isHover = case model.searchMenu of
-      ResultOpen maybeId -> maybeId == Just topicId
-      ResultClosed -> False
-  in
-  [ style "color" (if isHover then "white" else "black")
-  , style "background-color" (if isHover then "black" else "white")
-  , style "overflow" "hidden"
-  , style "text-overflow" "ellipsis"
-  , style "padding" "0 8px"
-  ]
+    let
+        isHover =
+            case model.searchMenu of
+                ResultOpen maybeId ->
+                    maybeId == Just topicId
+
+                ResultClosed ->
+                    False
+    in
+    [ style "color"
+        (if isHover then
+            "white"
+
+         else
+            "black"
+        )
+    , style "background-color"
+        (if isHover then
+            "black"
+
+         else
+            "white"
+        )
+    , style "overflow" "hidden"
+    , style "text-overflow" "ellipsis"
+    , style "padding" "0 8px"
+    ]
 
 
 
 -- UPDATE
 
 
-updateSearch : SearchMsg -> Model -> (Model, Cmd Msg)
+updateSearch : SearchMsg -> Model -> ( Model, Cmd Msg )
 updateSearch msg model =
-  case msg of
-    SearchInput text -> (onSearchInput text model, Cmd.none)
-    OverItem topicId -> (onOverItem topicId model, Cmd.none)
-    OutItem _ -> (onOutItem model, Cmd.none)
-    ClickItem topicId -> model
-      |> revealTopic topicId (activeMap model)
-      |> closeResultMenu
-      |> storeModel
+    case msg of
+        SearchInput text ->
+            ( onSearchInput text model, Cmd.none )
+
+        OverItem topicId ->
+            ( onOverItem topicId model, Cmd.none )
+
+        OutItem _ ->
+            ( onOutItem model, Cmd.none )
+
+        ClickItem topicId ->
+            model
+                |> revealTopic topicId (activeMap model)
+                |> closeResultMenu
+                |> storeModel
 
 
 onSearchInput : String -> Model -> Model
 onSearchInput text model =
-  { model | searchText = text } |> search
+    { model | searchText = text } |> search
 
 
 onOverItem : Id -> Model -> Model
 onOverItem topicId model =
-  case model.searchMenu of
-    ResultOpen _ ->
-      { model | searchMenu = ResultOpen (Just topicId) } -- update hovered topic
-    ResultClosed ->
-      logError "onOverItem" "Received \"OverItem\" message when searchMenu is ResultClosed"
-      model
+    case model.searchMenu of
+        ResultOpen _ ->
+            { model | searchMenu = ResultOpen (Just topicId) }
+
+        -- update hovered topic
+        ResultClosed ->
+            logError "onOverItem"
+                "Received \"OverItem\" message when searchMenu is ResultClosed"
+                model
 
 
 onOutItem : Model -> Model
 onOutItem model =
-  case model.searchMenu of
-    ResultOpen _ ->
-      { model | searchMenu = ResultOpen Nothing } -- update hovered topic
-    ResultClosed ->
-      logError "onOutItem" "Received \"OutItem\" message when searchMenu is ResultClosed"
-      model
+    case model.searchMenu of
+        ResultOpen _ ->
+            { model | searchMenu = ResultOpen Nothing }
+
+        -- update hovered topic
+        ResultClosed ->
+            logError "onOutItem"
+                "Received \"OutItem\" message when searchMenu is ResultClosed"
+                model
 
 
 search : Model -> Model
 search model =
-  { model
-  | searchResult = model.items |> Dict.foldr
-    (\id item topicIds ->
-      case item of
-        Topic {text} ->
-          if isMatch model.searchText text then
-            id :: topicIds
-          else
-            topicIds
-        Assoc _ -> topicIds
-    )
-    []
-  , searchMenu = ResultOpen Nothing
-  }
+    { model
+        | searchResult =
+            model.items
+                |> Dict.foldr
+                    (\id item topicIds ->
+                        case item of
+                            Topic { text } ->
+                                if isMatch model.searchText text then
+                                    id :: topicIds
+
+                                else
+                                    topicIds
+
+                            Assoc _ ->
+                                topicIds
+                    )
+                    []
+        , searchMenu = ResultOpen Nothing
+    }
 
 
 isMatch : String -> String -> Bool
 isMatch searchText text =
-  String.contains (String.toLower searchText) (String.toLower text)
+    String.contains (String.toLower searchText) (String.toLower text)
 
 
 revealTopic : Id -> MapId -> Model -> Model
 revealTopic topicId mapId model =
-  if isItemInMap topicId mapId model then
-    let
-      _ = info "revealTopic" (topicId, "set visible")
-    in
-    showItem topicId mapId model
-  else
-    let
-      _ = info "revealTopic" (topicId, "add to map")
-      props = MapTopic <| defaultProps topicId topicSize model
-    in
-    addItemToMap topicId props mapId model
+    if isItemInMap topicId mapId model then
+        let
+            _ =
+                info "revealTopic" ( topicId, "set visible" )
+        in
+        showItem topicId mapId model
+
+    else
+        let
+            _ =
+                info "revealTopic" ( topicId, "add to map" )
+
+            props =
+                MapTopic <| defaultProps topicId topicSize model
+        in
+        addItemToMap topicId props mapId model
 
 
 closeResultMenu : Model -> Model
 closeResultMenu model =
-  { model | searchMenu = ResultClosed }
+    { model | searchMenu = ResultClosed }
