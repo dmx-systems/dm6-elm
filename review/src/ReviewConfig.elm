c3bf6cfd0b9bcd36114518e2fb33c5b6dd1ebdab
RalfBarkow <ralf.barkow@me.com>
2025-08-27 09:36:38 +0200

    review: replace invalid CallGraph import with ExtractCallGraph
    - Stop ignoring the review/ folder in .gitignore
    - Fix ReviewConfig to use ExtractCallGraph.rule directly

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,13 +1,9 @@
 module ReviewConfig exposing (config)
 
+import ExtractCallGraph
 import Review.Rule exposing (Rule)
-import Some.CallGraph.Rule as CallGraph
 
 
 config : List Rule
 config =
-    [ CallGraph.rule
-        -- give the extract a stable name; this is what shows under .extracts.<name>
-        |> Review.Rule.withExtract "ExtractCallGraph"
-        |> Review.Rule.toRule
-    ]
+    [ ExtractCallGraph.rule ]
