dda70f41fe1e04c4aac67f424f13862edf7b5013
RalfBarkow <ralf.barkow@me.com>
2025-09-01 07:28:37 +0200

    chore(format): normalize Elm formatting

--- src/AppModel.elm
+++ src/AppModel.elm
@@ -1,59 +1,67 @@
 module AppModel exposing (..)
 
-import Model exposing (..)
 -- components
+
+import Dict
 import IconMenu exposing (IconMenuModel, IconMenuMsg)
+import Model exposing (..)
 import Mouse exposing (MouseModel, MouseMsg)
 import Search exposing (SearchModel, SearchMsg)
 
-import Dict
-
-
 
 type alias Model =
-  { items : Items -- TODO: represent container content independent from maps?
-  , maps : Maps
-  , mapPath : MapPath
-  , nextId : Id
-  ----- transient -----
-  , selection : Selection
-  , editState : EditState
-  , measureText : String
-  -- components
-  , mouse : MouseModel
-  , search : SearchModel
-  , iconMenu : IconMenuModel
-  }
+    { items : Items -- TODO: represent container content independent from maps?
+    , maps : Maps
+    , mapPath : MapPath
+    , nextId : Id
+
+    ----- transient -----
+    , selection : Selection
+    , editState : EditState
+    , measureText : String
+
+    -- components
+    , mouse : MouseModel
+    , search : SearchModel
+    , iconMenu : IconMenuModel
+    }
 
 
 default : Model
 default =
-  { items = Dict.empty
-  , maps = Dict.singleton 0 -- map 0 is the "home map", it has no corresponding topic
-    <| Map 0 -1 (Rectangle 0 0 0 0) Dict.empty -- parentMapId = -1
-  , mapPath = [0]
-  , nextId = 1
-  ----- transient -----
-  , selection = []
-  , editState = NoEdit
-  , measureText = ""
-  -- components
-  , mouse = Mouse.init
-  , search = Search.init
-  , iconMenu = IconMenu.init
-  }
+    { items = Dict.empty
+    , maps =
+        Dict.singleton 0
+        -- map 0 is the "home map", it has no corresponding topic
+        <|
+            Map 0 -1 (Rectangle 0 0 0 0) Dict.empty
+
+    -- parentMapId = -1
+    , mapPath = [ 0 ]
+    , nextId = 1
+
+    ----- transient -----
+    , selection = []
+    , editState = NoEdit
+    , measureText = ""
+
+    -- components
+    , mouse = Mouse.init
+    , search = Search.init
+    , iconMenu = IconMenu.init
+    }
 
 
 type Msg
-  = AddTopic
-  | MoveTopicToMap Id MapId Point Id MapId Point -- start point, random point (for target)
-  | SwitchDisplay DisplayMode
-  | Edit EditMsg
-  | Nav NavMsg
-  | Hide
-  | Delete
-  | NoOp
-  -- components
-  | Mouse MouseMsg
-  | Search SearchMsg
-  | IconMenu IconMenuMsg
+    = AddTopic
+    | MoveTopicToMap Id MapId Point Id MapId Point -- start point, random point (for target)
+    | SwitchDisplay DisplayMode
+    | Edit EditMsg
+    | Nav NavMsg
+    | Hide
+    | Delete
+    | NoOp
+      -- components
+    | Mouse MouseMsg
+    | Search SearchMsg
+    | IconMenu IconMenuMsg
